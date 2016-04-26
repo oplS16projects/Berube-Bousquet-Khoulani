e6deebbad94f22b450eadfd63548dcbd9b3b162b
Mark Sherman <msherman@cs.uml.edu>
2016-04-26 17:31:04 -0400

    updating V2 to latest code

--- clientV2.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-
-
-(define-values (in out) (tcp-connect "localhost" 9876))
-(write "move game 3 5 4 4" out)
-(flush-output out)
-(close-input-port in)
-(close-output-port out)
--- serverV2.rkt
+++ serverV2.rkt
@@ -1,6 +1,11 @@
 #lang racket
 (require racket/tcp)
-(require 2htdp/image)
+
+
+
+
+(define-namespace-anchor anc)
+(define ns (namespace-anchor->namespace anc))
 
 (define the-listener (tcp-listen 9876))
 (define-values (in out) (tcp-accept the-listener))
@@ -8,11 +13,11 @@
  (begin
     (displayln in-str)
     (string? in-str)
-    (eval (map (lambda (z) (if (string->number z) (string->number z) (string->symbol z))) (string-split in-str)))))
+    (eval (map (lambda (z) (if (string->number z) (string->number z) (string->symbol z))) (string-split in-str))ns)))
 (tcp-close the-listener)
 
 
 
 ;(map (lambda (z) (if (string->number z) (string->number z) (string->symbol z))) (string-split "move game 3 3 4 5"))
 ;'(move game 3 3 4 5)
-;> (eval (map (lambda (z) (if (string->number z) (string->number z) (string->symbol z))) (string-split "move game 3 3 4 5")))
+;> (eval (map (lambda (z) (if (string->number z) (string->number z) (string->symbol z))) (string-split "move game 3 5 4 4")))
