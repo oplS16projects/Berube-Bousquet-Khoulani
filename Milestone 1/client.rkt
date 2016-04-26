dc7acd43c5101cd989397349b7cec1cbe7c24fc0
sberube484 <sean_berube@student.uml.edu>
2016-04-26 17:22:40 -0400

    Cleaned up project, milestone folders removed

--- Milestone 1/client.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-
-
-(define-values (in out) (tcp-connect "localhost" 9876))
-(write "Hello" out)
-(flush-output out)
-(close-input-port in)
-(close-output-port out)
--- Milestone 1/server.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
- (require racket/tcp)
-
-
-(define the-listener (tcp-listen 9876))
-(define-values (in out) (tcp-accept the-listener))
-(displayln (read in))
-(tcp-close the-listener)
