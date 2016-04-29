87b6ade6d732938789bf4475a18460887cd04a2b
skhoulan <samir_khoulani@student.uml.edu>
2016-04-29 19:37:45 -0400

    Fixed all double jump conditions in advanced Ai

--- checkersV4.rkt
+++ checkersV4.rkt
@@ -235,11 +235,9 @@
 
           
                ;checking for double jump down left-right possibility
-               ((and (< i 8) 
-                     (< (+ i 2) 8)
-                     (< j 6)
+               ((and (< (+ i 2) 8)
                      (>= i 1)
-                     (<= j 5)
+                     (<= j 3)
                      (equal? (get-state board (- i 1) (+ j 1)) 'P1)
                      (equal? (get-state board (- i 1) (+ j 3)) 'P1))
                 (if (and (equal? (get-state board (- i 2) (+ 2 j)) 'BLANK)
@@ -251,11 +249,23 @@
                     #f))
 
                ;checking for double jump down right-left possibility
-               ((and (< i 8) 
-                     (< (+ i 2) 8)
-                     (< j 6)
+               ((and (< (+ i 3) 8)
                      (>= i 1)
-                     (<= j 5)
+                     (<= j 3)
+                     (equal? (get-state board (+ i 1) (+ j 1)) 'P1)
+                     (equal? (get-state board (+ i 1) (+ j 3)) 'P1))
+                (if (and (equal? (get-state board (+ i 2) (+ 2 j)) 'BLANK)
+                         (equal? (get-state board i (+ j 4)) 'BLANK))
+                    ((lambda args (set-state board i j 'BLANK)
+                       (set-state board (+ i 1) (+ j 1) 'BLANK) ; first piece to be jumped
+                       (set-state board (+ i 3) (+ j 3) 'BLANK) ; second piece to be jumped
+                       (set-state board (+ i 4) (+ j 4) 'P2)))  ; where AI lands after jumping two pieces
+                    #f))
+
+
+                ;checking for double jump down right-left possibility
+               ((and (< (+ i 4) 8)
+                     (< (+ j 4) 8)
                      (equal? (get-state board (+ i 1) (+ j 1)) 'P1)
                      (equal? (get-state board (+ i 1) (+ j 3)) 'P1))
                 (if (and (equal? (get-state board (+ i 2) (+ 2 j)) 'BLANK)
@@ -267,11 +277,8 @@
                     #f))
 
                ;checking for double jump down diag-right possibility
-               ((and (< i 8) 
-                     (< (+ i 2) 8)
-                     (< j 6)
-                     (>= i 1)
-                     (<= j 5)
+               ((and (< (+ i 4) 8)
+                     (< (+ j 4) 8)
                      (equal? (get-state board (+ i 1) (+ j 1)) 'P1)
                      (equal? (get-state board (+ i 3) (+ j 3)) 'P1))
                 (if (and (equal? (get-state board (+ i 2) (+ j 2)) 'BLANK)
